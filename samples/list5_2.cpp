4318e39593e768ab0bdb9cc9b9845faa3cd55e36
Kentkn192837 <agriculture5012@gmail.com>
2022-01-04 09:13:00 +0900

    sample

---
+++ samples/list5_2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    int ans;
+    std::string s = "Hello, world";
+    ans = s.length();
+
+    std::cout << ans << std::endl;
+    return 0;
+}

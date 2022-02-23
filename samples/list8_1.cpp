039384f793864214bdfb16c8a5c8f54fe9cb7cd0
Kentkn192837 <agriculture5012@gmail.com>
2022-02-23 23:36:06 +0900

    sample

---
+++ samples/list8_1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+class MyClass
+{
+    private:
+        int data;
+    
+    public:
+        int getData()
+        {
+            return this->data;
+        }
+
+        MyClass(int data)
+        {
+            std::cout << "コンストラクタが呼び出されました。" << std::endl;
+            this->data = data;
+        }
+
+        ~MyClass()
+        {
+            std::cout << "デストラクタが呼び出されました。" << std::endl;
+        }
+};
+
+int main()
+{
+    MyClass obj1(123);
+    MyClass obj2 = obj1;
+    std::cout << "obj1のメンバ変数dataの値:" << obj1.getData() << std::endl;
+    std::cout << "obj2のメンバ変数dataの値:" << obj2.getData() << std::endl;
+}

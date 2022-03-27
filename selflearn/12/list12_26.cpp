2f03c544fa6b2e680a8fbf3ce8e88cf29af5dcf6
Kentkn192837 <agriculture5012@gmail.com>
2022-03-27 22:28:14 +0900

    std::setの検索

--- selflearn/12/list12_26.cpp
+++ selflearn/12/list12_26.cpp
@@ -5,4 +5,13 @@ int main()
 {
     // std::setは要素の重複がない集合
     std::set is = { 5, 3, 1, 7 };
+
+    for (int i = 0; i < 8; i++) {
+        auto iter = is.find(i);
+        if (iter != is.end()) {
+            std::cout << *iter << std::endl;
+        } else {
+            std::cout << "? " << std::endl;
+        }
+    }
 }

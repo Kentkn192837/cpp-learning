a0999c1501d8a2c8066269774e3c89da3f167742
Kentkn192837 <agriculture5012@gmail.com>
2022-05-01 21:26:49 +0900

    sample

---
+++ selflearn/13/list13_13.cpp
@@ -0,0 +1,18 @@
+/* g++ -std=c++17 list13_13 list13_13.cpp */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "ushikamoshika";
+    std::string sub = "shika";
+
+    std::cout << str.find(sub) << std::endl;
+    std::cout << str.find(sub, 2) << std::endl;
+
+    std::cout << str.rfind(sub) << std::endl;
+    std::cout << str.rfind(sub, 2) << std::endl;
+    
+    std::cout << str.find('s') << std::endl;
+    std::cout << str.rfind('s') << std::endl;
+}

950a0dadacd7fbca8879871d62fc8ed4dafb853d
Kentkn192837 <agriculture5012@gmail.com>
2022-05-04 01:47:23 +0900

    sample

---
+++ selflearn/13/list13_14.cpp
@@ -0,0 +1,19 @@
+/* g++ -std=c++17 list13_14 list13_14.cpp */
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string str = "C,C++,Java,Ruby.";
+    std::string delim = ",";
+
+    std::string::size_type fpos = 0;
+    while ((fpos = str.find_first_not_of(delim, fpos)) != std::string::npos) {
+        auto lpos = str.find_first_not_of(delim, fpos);
+
+        std::string token(str, fpos, lpos - fpos);
+        std::cout << "[" << token <<"] ";
+        fpos = lpos;
+    }
+    std::cout << std::endl;
+}

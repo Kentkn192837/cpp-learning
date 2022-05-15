345fb78248f0a3f089caa37b06712c2763f2c38f
Kentkn192837 <agriculture5012@gmail.com>
2022-05-15 21:32:23 +0900

    sample

---
+++ c/11/funcptr01.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int func(int);
+
+int main(void)
+{
+    printf("main関数のアドレス = %p\n", main);
+    printf("func関数のアドレス = %p\n", func);
+    return 0;
+}
+
+int func(int x)
+{
+    printf("x = %d\n", x);
+    return 0;
+}

1f704ca14a14d2ae326579515f0164d14a82b75e
Kentkn192837 <agriculture5012@gmail.com>
2022-05-23 21:26:53 +0900

    to_string関数

---
+++ selflearn/13/list13_16.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string integer = std::to_string(42);
+    std::string floating_point = std::to_string(3.1415926);
+    std::cout << integer << std::endl;
+    std::cout << floating_point << std::endl;
+}
